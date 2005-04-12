736119149a04ed68ccaa194b3a8a1da4290e31f8
Michael G Schwern <mschwern@cpan.org>
2005-04-12 23:23:53 +0000

    Import of MSCHWERN/ExtUtils-MakeMaker-6.28 from CPAN.
    gitpan-cpan-distribution: ExtUtils-MakeMaker
    gitpan-cpan-version:      6.28
    gitpan-cpan-path:         MSCHWERN/ExtUtils-MakeMaker-6.28.tar.gz
    gitpan-cpan-author:       MSCHWERN
    gitpan-cpan-maturity:     released

--- lib/ExtUtils/MM_AIX.pm
+++ lib/ExtUtils/MM_AIX.pm
@@ -2,10 +2,12 @@ package ExtUtils::MM_AIX;
 
 use strict;
 use vars qw($VERSION @ISA);
-$VERSION = '0.02';
+$VERSION = '0.03';
 
 require ExtUtils::MM_Unix;
 @ISA = qw(ExtUtils::MM_Unix);
+
+use ExtUtils::MakeMaker qw(neatvalue);
 
 
 =head1 NAME
