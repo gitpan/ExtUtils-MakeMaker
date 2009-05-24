4a4516ed11cdd3be9912542f337f79be75a605da
Michael G Schwern <mschwern@cpan.org>
2009-05-24 21:07:28 +0000

    Import of MSCHWERN/ExtUtils-MakeMaker-6.51_04 from CPAN.
    gitpan-cpan-distribution: ExtUtils-MakeMaker
    gitpan-cpan-version:      6.51_04
    gitpan-cpan-path:         MSCHWERN/ExtUtils-MakeMaker-6.51_04.tar.gz
    gitpan-cpan-author:       MSCHWERN
    gitpan-cpan-maturity:     developer

--- lib/ExtUtils/MM_MacOS.pm
+++ lib/ExtUtils/MM_MacOS.pm
@@ -2,7 +2,7 @@ package ExtUtils::MM_MacOS;
 
 use strict;
 
-our $VERSION = 6.51_03;
+our $VERSION = 6.51_04;
 
 sub new {
     die <<'UNSUPPORTED';
--- lib/ExtUtils/MM_VMS.pm
+++ lib/ExtUtils/MM_VMS.pm
@@ -15,7 +15,7 @@ BEGIN {
 
 use File::Basename;
 
-our $VERSION = '6.51_03';
+our $VERSION = '6.51_04';
 
 require ExtUtils::MM_Any;
 require ExtUtils::MM_Unix;
